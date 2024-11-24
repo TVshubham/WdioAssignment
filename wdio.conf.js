710b285a286be4369333cf92fff807004d7642c5
TVshubham <Shubhamsn@testvagrant.com>
2024-11-24 13:28:12 +0530

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -56,7 +56,10 @@ exports.config = {
     // https://saucelabs.com/platform/platform-configurator
     //
     capabilities: [{
-        browserName: 'chrome'
+        browserName: 'chrome',
+        'goog:chromeOptions': {
+      args: ['--headless', '--disable-gpu', '--no-sandbox', '--disable-dev-shm-usage'],
+    },
     }],
 
     //
